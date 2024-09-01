7dc5271d1b0281ac26469cf8b72680832f8df8fd
Ruhal-Doshi <doshi.ruhal@gmail.com>
2024-09-01 19:35:53 +0530

    final google mapping

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -15,7 +15,6 @@ export const auth = defineAuth({
         attributeMapping: {
           email: 'email',
           preferredUsername: 'name',
-          profilePage: 'profile'
         }
       },
       callbackUrls: [
